c698060cd6239e1051bb78aa262609d6594c3dd5
sophie <sophiezp2007@gmail.com>
2026-02-28 13:38:40 -0500

    fix(style): The landing page is working

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://SophieZP.github.io',
   base: '/CFGTWPO',
   output: 'static',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    css: {
+      transformer: 'lightningcss',
+    },
+  },
 });
